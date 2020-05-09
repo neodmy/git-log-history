3473b98428602a9636570882fa271b244e4f0569
David Yusta <david.yusta@guidesmiths.com>
2020-05-09 22:57:35 +0200

    fix: remove default options in yargs

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const { renderLog } = require('./lib/generator');
 const options = yargs
   .scriptName('git-log-history')
   .wrap(null)
-  .usage('Usage: $0 -f <file> [options]')
+  .usage('Usage: $0 [options]')
   .alias('h', 'help')
   .option('f', {
-    alias: 'file', describe: 'Relative path from root directory to pull request template', type: 'string', demandOption: true,
+    alias: 'file', describe: 'Relative path from root directory to pull request template', type: 'string',
   })
   .option('p', {
-    alias: 'pattern', describe: 'Pattern to replace in target file', type: 'string', default: '<% changelog %>',
+    alias: 'pattern', describe: 'Pattern to replace in target file. Default: "<% changelog %>"', type: 'string',
   })
   .option('l', {
-    alias: 'log', describe: 'git log --pretty=<log>', type: 'string', default: 'format:- %h %s by <username_or_name>',
+    alias: 'log', describe: 'git log --pretty=<log>. Default: format:- %h %s by <username_or_name>', type: 'string',
   })
   .example('$0 -f .github/pull_request_templat.md',
     'Replace <% changelog %> in .github/pull_request_temaplate.md with commit history formatted with --pretty=format:- %h %s by <username_or_name>')
