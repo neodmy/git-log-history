2f3c33ea138550886a11fb476ba4480501efd4b8
David Yusta <david.yusta@guidesmiths.com>
2020-05-09 23:18:09 +0200

    feat: add example for stdout

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const options = yargs
   .option('l', {
     alias: 'log', describe: 'Options to be append to git log --pretty=<log>. Default: format:- %h %s by <username_or_name>', type: 'string', requiresArg: true,
   })
+  .example('$0',
+    'Prints to stdout commit history formatted with --pretty=format:- %h %s by <username_or_name>')
   .example('$0 -f .github/pull_request_templat.md',
     'Replace <% changelog %> in .github/pull_request_temaplate.md with commit history formatted with --pretty=format:- %h %s by <username_or_name>')
   .example('$0 -f someDir/someFile.md -p "<? something ¿>" -l short',
