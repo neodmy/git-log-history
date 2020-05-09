f76edff9e9c05ced400c5c53a9c5b46e3afb99f0
David Miguel Yusta <36865163+neodmy@users.noreply.github.com>
2020-05-09 13:06:02 +0200

    Log options (#6)
    * feat: add git log pretty config and handle master branch logs
    
    * feat: add git log pretty options to render
    
    * feat: add log arg and change f option description

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const { renderLog } = require('./lib/generator');
 const options = yargs
   .usage('Usage: -f <file>')
   .option('f', {
-    alias: 'file', describe: 'Absolute path to pull request template', type: 'string', demandOption: true,
+    alias: 'file', describe: 'Relative path from root directory to pull request template', type: 'string', demandOption: true,
   })
   .option('p', {
     alias: 'pattern', describe: 'Pattern to replace. Default: "<% changelog %>"', type: 'string',
   })
+  .option('l', {
+    alias: 'log', describe: 'git log --pretty=<log>. Default: --pretty=format:- %h %s by <username | user>', type: 'string',
+  })
   .argv;
 
-renderLog(options.f, options.p);
+renderLog(options.f, options.p, options.l);
